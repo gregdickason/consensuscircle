cca27baae8ae88863bc1db500bd16f4b0ac18e2f
Cameron McEwan <cameronpmcewan@gmail.com>
2018-11-06 19:26:53 +1100

    System ready for GKE

--- gkeshutdown.sh
+++ gkeshutdown.sh
@@ -9,9 +9,5 @@ fi
 echo INFO: This script assumes you have launched on GKE if you have not then you will receive errors
 
 gcloud container clusters get-credentials $1
-kubectl delete service cc-client
-kubectl delete deployment cc-client
-kubectl delete service rq-worker
-kubectl delete deployment rq-worker
-kubectl delete service redis
-kubectl delete deployment redis
+kubectl delete service cc-client rq-worker redis
+kubectl delete deployment cc-client rq-worker redis 
