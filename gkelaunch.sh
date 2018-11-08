303ca7425e5efd2b23110dd65b5b796b062f918a
Cameron McEwan <cameronpmcewan@gmail.com>
2018-11-08 14:12:47 +1100

    Tidy up build scripts

--- gkelaunch.sh
+++ gkelaunch.sh
@@ -7,9 +7,9 @@ exit 1
 fi
 
 gcloud container clusters get-credentials $1
-kubectl create -f deploymentScripts/redis-deployment.yaml
-kubectl create -f deploymentScripts/redis-service.yaml
-kubectl create -f deploymentScripts/rq-worker-deployment.yaml
-kubectl create -f deploymentScripts/rq-worker-service.yaml
-kubectl create -f deploymentScripts/cc-client-deployment.yaml
+kubectl create -f buildScripts/redis-deployment.yaml
+kubectl create -f buildScripts/redis-service.yaml
+kubectl create -f buildScripts/rq-worker-deployment.yaml
+kubectl create -f buildScripts/rq-worker-service.yaml
+kubectl create -f buildScripts/cc-client-deployment.yaml
 kubectl expose deployment cc-client --type=LoadBalancer --port 5000 --target-port 5000
